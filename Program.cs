00be3211457c5722c90d58e25995c4103bcb3886
bagnz0r <bagnohax@gmail.com>
2013-06-14 13:39:12 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dot_clean
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\ndot_clean for .NET v1.0\n(c) 2013 by bagnz0r <http://github.com/bagnz0r>n\n");
+
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Usage: dot_clean <path>\n\n");
+            }
+
+            string path = args[0];
+
+            Console.WriteLine("Looking for junk files... (Be patient)\n");
+            string[] dotUnderscore = Directory.GetFiles(path, "._*", SearchOption.AllDirectories);
+            string[] dsStore = Directory.GetFiles(path, ".DS_Store", SearchOption.AllDirectories);
+
+            Console.WriteLine("Found " + (dotUnderscore.Length + dsStore.Length).ToString() + " junk files!\n\n");
+
+            try
+            {
+                foreach (string dotFile in dotUnderscore)
+                {
+                    if (Directory.Exists(dotFile))
+                    {
+                        Directory.Delete(dotFile);
+                    }
+                    else
+                    {
+                        File.Delete(dotFile);
+                    }
+
+                    Console.WriteLine("Removed '" + dotFile + "'...\n");
+                }
+
+                foreach (string dsFile in dsStore)
+                {
+                    if (Directory.Exists(dsFile))
+                    {
+                        Directory.Delete(dsFile);
+                    }
+                    else
+                    {
+                        File.Delete(dsFile);
+                    }
+
+                    Console.WriteLine("Removed '" + dsFile + "'...\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("The program has failed to run!");
+                File.WriteAllText("error.log", ex.Message + ": " + ex.StackTrace);
+            }
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("dot_clean")]
+[assembly: AssemblyDescription("dot_clean for .NET")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("bagnz0r")]
+[assembly: AssemblyProduct("dot_clean for .NET")]
+[assembly: AssemblyCopyright("Copyright © bagnz0r 2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5261c792-a595-4417-bf40-3b89c8850732")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
