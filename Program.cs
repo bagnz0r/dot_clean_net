5e67c71556d18c456c9f1858ce704f50442834c0
bagnz0r <bagnohax@gmail.com>
2013-06-20 17:39:35 +0200

    Improved code quality, fixed several bugs and added more files to the shitlist. Also included pre-built binaries.

--- Program.cs
+++ Program.cs
@@ -9,52 +9,38 @@ namespace dot_clean
 {
     class Program
     {
+        /// <summary>
+        /// Program entry point.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            Console.WriteLine("\ndot_clean for .NET v1.0\n(c) 2013 by bagnz0r <http://github.com/bagnz0r>n\n");
+            Console.WriteLine("\ndot_clean for .NET v1.0\n(c) 2013 by bagnz0r <http://github.com/bagnz0r>\n\n");
 
             if (args.Length < 1)
             {
                 Console.WriteLine("Usage: dot_clean <path>\n\n");
             }
 
+            // Take path and verify that it exists.
             string path = args[0];
+            if (Directory.Exists(path) != true)
+            {
+                Console.WriteLine("Path doesn't exist.");
+                Environment.Exit(1);
+            }
 
+            // List all possible junk files here.
             Console.WriteLine("Looking for junk files... (Be patient)\n");
-            string[] dotUnderscore = Directory.GetFiles(path, "._*", SearchOption.AllDirectories);
-            string[] dsStore = Directory.GetFiles(path, ".DS_Store", SearchOption.AllDirectories);
+            List<string> junkFiles = FindJunkFiles(path);
 
-            Console.WriteLine("Found " + (dotUnderscore.Length + dsStore.Length).ToString() + " junk files!\n\n");
+            // Spam with some statistical bullshit.
+            Console.WriteLine("Found " + (junkFiles.Count).ToString() + " junk files!\n\n");
 
+            // Delete all 'em suckers.
             try
             {
-                foreach (string dotFile in dotUnderscore)
-                {
-                    if (Directory.Exists(dotFile))
-                    {
-                        Directory.Delete(dotFile);
-                    }
-                    else
-                    {
-                        File.Delete(dotFile);
-                    }
-
-                    Console.WriteLine("Removed '" + dotFile + "'...\n");
-                }
-
-                foreach (string dsFile in dsStore)
-                {
-                    if (Directory.Exists(dsFile))
-                    {
-                        Directory.Delete(dsFile);
-                    }
-                    else
-                    {
-                        File.Delete(dsFile);
-                    }
-
-                    Console.WriteLine("Removed '" + dsFile + "'...\n");
-                }
+                DeleteJunkFiles(junkFiles);
             }
             catch (Exception ex)
             {
@@ -63,5 +49,78 @@ namespace dot_clean
             }
 
         }
+
+        /// <summary>
+        /// Finds and returns all the junk files possible.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private static List<string> FindJunkFiles(string path)
+        {
+            List<string> junkFiles = new List<string>();
+
+            // Find all the possible junk files that occur in every directory (OS X specific).
+            AddAllItemsToList(junkFiles, Directory.GetFiles(path, ".com.apple*", SearchOption.AllDirectories));
+            AddAllItemsToList(junkFiles, Directory.GetFiles(path, "._*", SearchOption.AllDirectories));
+            AddAllItemsToList(junkFiles, Directory.GetFiles(path, ".DS_Store", SearchOption.AllDirectories));
+            AddAllItemsToList(junkFiles, Directory.GetFiles(path, ".__MACOSX", SearchOption.AllDirectories));
+
+            // Find all the possible junk files that occur in every directory (Windows specific).
+            AddAllItemsToList(junkFiles, Directory.GetFiles(path, "Thumbs.db", SearchOption.AllDirectories));
+
+            // Find all the possible junk folders that occur only in top path (OS X specific).
+            AddAllItemsToList(junkFiles, Directory.GetFiles(path, ".Spotlight-V100", SearchOption.TopDirectoryOnly));
+            AddAllItemsToList(junkFiles, Directory.GetFiles(path, ".TemporaryItems", SearchOption.TopDirectoryOnly));
+            AddAllItemsToList(junkFiles, Directory.GetFiles(path, ".Trashes", SearchOption.TopDirectoryOnly));
+            AddAllItemsToList(junkFiles, Directory.GetFiles(path, ".fseventsd", SearchOption.AllDirectories));
+
+            return junkFiles;
+        }
+
+        /// <summary>
+        /// Deletes all the junk files and logs the progress.
+        /// </summary>
+        /// <param name="junkFiles"></param>
+        private static void DeleteJunkFiles(List<string> junkFiles)
+        {
+            StreamWriter logWriter = new StreamWriter("dot_clean.log");
+
+            foreach (string junkFile in junkFiles)
+            {
+                if (Directory.Exists(junkFile))
+                {
+                    Directory.Delete(junkFile);
+                }
+                else
+                {
+                    File.Delete(junkFile);
+                }
+
+                string removedMsg = "Removed '" + junkFile + "'...";
+                Console.WriteLine(removedMsg + "\n");
+                logWriter.WriteLine(removedMsg);
+            }
+
+            logWriter.Flush();
+            logWriter.Dispose();
+        }
+
+
+        /// <summary>
+        /// Loops over all the items in an array and adds them to referenced list.
+        /// </summary>
+        /// <param name="list"></param>
+        /// <param name="items"></param>
+        /// <returns></returns>
+        private static List<string> AddAllItemsToList(List<string> list, string[] items)
+        {
+            foreach (string item in items)
+            {
+                list.Add(item);
+            }
+
+            return list;
+        }
+
     }
 }
